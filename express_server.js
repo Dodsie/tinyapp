74f471c2cb3816cdc5d091752a44f9f38f871de5
Dodsie <baileydods@gmail.com>
2022-03-21 16:28:56 -0600

    Added and implented header partials within ./views/partials/header.ejs

--- express_server.js
+++ express_server.js
@@ -20,7 +20,6 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  console.log(req.params);
   res.render("urls_show", templateVars);
 });
 
