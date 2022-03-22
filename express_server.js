7fe565767bb26aaa77813d49f7cf6a2e800350d0
Dodsie <baileydods@gmail.com>
2022-03-22 16:32:22 -0600

    Added delete button to urls

--- express_server.js
+++ express_server.js
@@ -1,35 +1,39 @@
+// app configuration
 const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
 
+//functon for generating new string.
 const generateRandomString = () => {
   let string = Math.random().toString(36).slice(7);
   return string;
 };
-
+// Database Variable
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}; // Need to save after submission here.
+};
 
+
+// Browse Responses.
+//home page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+// urls page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+// urls/new page, create a new shortUrl
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
@@ -37,20 +41,36 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+// this is adding the shortURL : longURL in object form rendering it within
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
+// response when requesting localhost:port/hello
 app.get("/hello", (req, res) => {
   res.send("<html><body> Hello <b> World</></body></html>\n");
 });
-
+//redirect to longURL when short URL is applied in the /u/:shortURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+//delete
+app.post("/urls/:shortURL/delete", (req, res) => {
+  let shortURL = req.params.shortURL;
+  for (let url in urlDatabase) {
+    if (url === shortURL) {
+      delete urlDatabase[shortURL];
+      res.redirect("/urls");
+    }
+  }
+});
+
+
+
+// listens on port specified, returns a log when sucessfully listening.
 app.listen(PORT,() => {
   console.log(`Example app listening on ${PORT}!`);
 });
